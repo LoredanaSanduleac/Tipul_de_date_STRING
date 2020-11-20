a6e912269012425e2b1226d28365ee64778f40ab
Loredana Sanduleac <70658391+LoredanaSanduleac@users.noreply.github.com>
2020-11-20 09:17:26 +0300

    Delete problema1a_string.py

--- problema1a_string.py
+++
@@ -1,6 +0,0 @@
-str=input("introduceti un sir de caractere: ")
-majuscule=0
-for i in range(len(str)):
-    if(str[i]>='A' and str[i]<='Z'):
-        majuscule+=1
-print('in sir sunt: ',majuscule, "majuscule")
